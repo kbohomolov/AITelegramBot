2e29cfee29e87fa8261386e54063d1647e2d627d
Kyrylo Bohomolov <kyrylo13bohomolov@gmail.com>
2025-02-11 21:02:12 +0200

    Created two main menu keyboards: main_admin_keyboard and main_user_keyboard in 'reply_keyboard.py' file

--- app/keyboards/reply_keyboard.py
+++ app/keyboards/reply_keyboard.py
@@ -1,9 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-main_keyboard = ReplyKeyboardMarkup(keyboard=[
+main_admin_keyboard = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='🤖 Почати діалог зі ШІ'),  KeyboardButton(text='⁉️ FAQ (Поширені питання)')],
     [KeyboardButton(text='⚙️ Налаштування'), KeyboardButton(text='🔐 Панель адміністратора')],
 ],  resize_keyboard=True)
 
-#],  input_field_placeholder='Оберіть потрібний пункт з меню нижче:', resize_keyboard=True)
+main_user_keyboard = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='🤖 Почати діалог зі ШІ'),  KeyboardButton(text='⁉️ FAQ (Поширені питання)')],
+    [KeyboardButton(text='⚙️ Налаштування')],
+],  resize_keyboard=True)
