66fb6cdb4daf3dab16b69e495c84d644bc385513
Kyrylo Bohomolov <kyrylo13bohomolov@gmail.com>
2025-02-16 15:52:13 +0200

    In models.py file, class User created two new fields (language and ai_model)

--- app/database/models.py
+++ app/database/models.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 
 
+load_dotenv()
+
 engine = create_async_engine(url=os.getenv('DB_URL'))
 async_session =  async_sessionmaker(engine)
 
@@ -18,19 +20,12 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False)
     telegram_id = mapped_column(BigInteger, unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(String(100))  
-    registration_date: Mapped[DateTime] = mapped_column(DateTime)
+    username: Mapped[str | None] = mapped_column(String(100))  
+    registration_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    language = mapped_column(String(20), default='ukrainian', nullable=False)
+    ai_model = mapped_column(String(50), default='Mistral AI', nullable=False)
     is_admin: Mapped[Boolean] = mapped_column(Boolean, default=False, nullable=False)
 
-class Admin(Base):
-    __tablename__ = 'admins'
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False)
-    telegram_id = mapped_column(BigInteger, unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(String(100))
-
-
 async def async_main():
-    load_dotenv()
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
